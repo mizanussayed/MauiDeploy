b76a17842705d51785ec6ec29ad0db7b93f8c0c9
Mizanus Sayed <mizandusp@gmail.com>
2024-04-24 23:39:44 +0600

    update develop branch

--- Platforms/iOS/AppDelegate.cs
+++
@@ -1,8 +0,0 @@
-﻿using Foundation;
-
-namespace MauiDeploy;
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
---
+++ src/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiDeploy;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
